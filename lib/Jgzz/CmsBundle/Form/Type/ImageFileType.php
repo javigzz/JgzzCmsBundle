1fb282f9ad45ab297afa3cdb436c816c8de01a0b
javier <javigzz@yahoo.es>
2012-09-09 17:08:44 +0200

    adapta ImageFileType a syfmony 2.1 primer commit de la version master separada de version v0.1

--- lib/Jgzz/CmsBundle/Form/Type/ImageFileType.php
+++ lib/Jgzz/CmsBundle/Form/Type/ImageFileType.php
@@ -2,24 +2,40 @@
 namespace Jgzz\CmsBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+//use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\OptionsResolver\Options;
 
+/**
+ * Adaptada a componente forms de syfmony 2.1
+ */
 class ImageFileType extends AbstractType
 {
-    public function getDefaultOptions(array $options)
-    {
-		return array(
-			'current_file' => null,
-			'max_width' => 200,
-			'width'	=> null,
-			'height' => null,
-		);
-    }
+  //   public function getDefaultOptions(array $options)
+  //   {
+		// return array(
+		// 	'current_file' => null,
+		// 	'max_width' => 200,
+		// 	'width'	=> null,
+		// 	'height' => null,
+		// );
+  //   }
+  //   
+  	public function setDefaultOptions(OptionsResolverInterface $resolver)
+{
+    $resolver->setDefaults(array(
+        'current_file' => null,
+		'height' => null,
+		'max_width' => 200,
+		'width'	=> null,
+    ));
+}
 
-    public function getParent(array $options)
+    public function getParent()
     {
         return 'file';
     }
@@ -29,7 +45,13 @@ class ImageFileType extends AbstractType
         return 'imagefile';
     }
 	
-	 public function buildForm(FormBuilder $builder, array $options)
+	/*
+	se ha sustituido interfaz de FormBuilder por FormBuilderInterface
+	por compatibilidad con sf 2.1, así como añadido array options en buildView
+	sin embargo esto no es suficiente para garantizar BC con sf 2.0 así que se 
+	optará por 
+	 */
+	public function buildForm(FormBuilderInterface $builder, array $options)
     {
     	$builder
             ->setAttribute('current_file', $options['current_file'])
@@ -37,7 +59,7 @@ class ImageFileType extends AbstractType
 			;
 	}
 	
-	public function buildView(FormView $view, FormInterface $form){
+	public function buildView(FormView $view, FormInterface $form, array $options){
 		
 		//$options = $this->getDefaultOptions(array());
 		
